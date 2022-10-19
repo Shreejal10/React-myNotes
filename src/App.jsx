d9962c1c5d41527576ac464ae4306618c452acee
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-19 22:21:25 +0545

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Navbar from "./components/Navbar"
-
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-
-
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='pt-16'>
+            <h1 className='text-center text-3xl text-white mt-3'>Hello, </h1>
+        </div>
+    )
+}
+
+export default Home
