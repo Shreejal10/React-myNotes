15ea88b8fc4a6b4eb1141f5648135a6d04b3ee05
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-20 23:18:44 +0545

    EditNote modal component

---
+++ src/components/EditNote.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const EditNote = (props) => {
+    return (
+        <>
+            <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+                </div>
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+                        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                            <div className="bg-[#13476e] px-6 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                <div className=" sm:flex sm:items-center flex flex-col gap-5  ">
+                                    <div className="mt-4 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                        <h3 className="text-2xl font-medium leading-6 text-white" id="modal-title">Edit Note</h3>
+                                    </div>
+                                    <form className="space-y-6 sm:w-1/2" action="#">
+                                        <div>
+                                            <label htmlFor="title" className="block mb-2 text-base font-medium text-white ">Title</label>
+                                            <input type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="description" className="block mb-2 text-base font-medium text-white ">Description</label>
+                                            <input type="text" name="description" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="password" className="block mb-2 text-base font-medium text-white">Tag</label>
+                                            <select name="tag" id="tag" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                                                <option value="Personal">Personal</option>
+                                                <option value="Learning">Learning</option>
+                                                <option value="College">College</option>
+                                                <option value="Others">Others</option>
+                                            </select>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className="bg-[#13476e] py-6 px-6 sm:flex sm:flex-row-reverse md:flex-row-reverse  items-center justify-center gap-1">
+                                <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-[#062f4e] px-2 py-2 text-base font-medium text-white shadow-sm hover:bg-[#051724] focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Edit Note</button>
+                                <button onClick={() => { props.setOpenModal(false) }} type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-2 py-2 text-base font-medium text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default EditNote
--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { RiDeleteBin5Line } from 'react-icons/ri'
 import { FiEdit } from 'react-icons/fi'
-
+import EditNote from '../components/EditNote'
 const NoteItem = () => {
+    const [openModal, setOpenModal] = useState(false);
     return (
         <>
+            {openModal && <EditNote setOpenModal={setOpenModal} />}
             <div className="p-4  md:w-1/2 lg:w-1/3">
                 <div className="h-full bg-[#13476e] bg-opacity-75 px-8 pt-9 pb-24 rounded-3xl overflow-hidden text-center relative">
                     <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-5 underline">TAG</h2>
@@ -14,7 +16,7 @@ const NoteItem = () => {
                         <span className="text-white mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200 ">
                             <RiDeleteBin5Line className='cursor-pointer' size={20} />
                         </span>
-                        <span className="text-white inline-flex items-center leading-none text-sm ">
+                        <span onClick={() => { setOpenModal(true) }} className="text-white inline-flex items-center leading-none text-sm ">
                             <FiEdit className=' cursor-pointer' size={20} />
                         </span>
                     </div>
