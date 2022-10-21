957d47559391ed4963d19ce740b938d972de1cbd
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-21 10:27:21 +0545

    style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div className='flex justify-between items-center p-4  w-full absolute bg-[#0a3557] h-16'>
             <h1 className='text-white text-3xl font-bold cursor-pointer'>myNotes</h1>
             <div className='flex justify-center items-center'>
-                <button className=' px-4 py-2 cursor-pointer text-white flex items-center gap-1 justify-center hover:text-[#5f8bac] hover:scale-110 active:text-[#A167A5]'><VscSignOut />Sign Out</button>
+                <button className=' px-4 py-2 cursor-pointer text-white flex items-center gap-1 justify-center hover:text-[#5f8bac] hover:scale-110'><VscSignOut />Sign Out</button>
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <div className='pt-16'>
                 <h1 className='text-center text-4xl text-white mt-3'>Hello, </h1>
                 <div className=' container mx-auto flex items-center justify-around py-4 mt-6'>
-                    <button onClick={() => { setOpenModal(true) }} className='text-white text-xl bg-[#062f4e] px-2 py-1 rounded-xl flex items-center justify-center gap-2' ><p>Add Note</p> < IoAddCircleOutline size={20} className="mt-1" /> </button>
+                    <button onClick={() => { setOpenModal(true) }} className='text-white text-xl bg-[#062f4e] hover:bg-[#051724] px-2 py-1 rounded-xl flex items-center justify-center gap-2' ><p>Add Note</p> < IoAddCircleOutline size={20} className="mt-1" /> </button>
                     <div className='flex justify-center items-center gap-2'>
                         <label htmlFor="notes" className='text-white text-xl'>Sort by: </label>
                         <select name="notes" id="notes" className='rounded-xl px-2 py-1  bg-[#062f4e] text-white '>
