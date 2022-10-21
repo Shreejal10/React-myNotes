4fbdd7ce8ca19d7bff3c380dbdce737315dfa070
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-21 15:58:13 +0545

    Added Login and Signup Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import Navbar from "./components/Navbar"
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import NoteState from "./context/notes/NoteState";
 function App() {
   return (
     <>
       <NoteState>
         <BrowserRouter>
-          <Navbar />
           <div>
             <Routes>
               <Route path="/" element={<Home />}></Route>
+              <Route index path="/login" element={<Login />}></Route>
+              <Route index path="/signup" element={<SignUp />}></Route>
             </Routes>
           </div>
         </BrowserRouter>
